7dd9c826a5f8129b68bd05b41c334f9cc58aaa20
Saima Bibi <bibi33243@gmail.com>
2025-01-16 14:16:18 +0500

    set up React Native expo Project

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/stack';
 
 export default function App() {
+
+  const stack = createNativeStackNavigator
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
     </View>
   );
 }
